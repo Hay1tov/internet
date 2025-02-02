bed457bd03d9faed2217bfb618d65de9fbb02bd0
Hay1tov <eldorh35@gmail.com>
2025-02-02 17:04:59 +0500

    Bajarildi

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import socket
 import json
+import mysql.connector
+import setting
+
 
 
 with open("domains.txt") as input_file:
@@ -8,12 +11,38 @@ with open("domains.txt") as input_file:
     result = []
     for domain in domains:
         ip = socket.gethostbyname(domain)
-        
+            
         result.append({
-            'name': domain,
-            'ip': ip
+            "name": domain,
+            "ip": ip
         })
 
 with open("domains.json", "w") as output_file:
     result_json = json.dumps(result, indent=4)
     output_file.write(result_json)
+    
+connection = mysql.connector.connect(
+    host=setting.host,
+    user=setting.user,
+    password=setting.password,
+    port=setting.port,
+    # database=setting.db_name
+)
+
+cursor = connection.cursor()
+
+cursor.execute("create database if not exists Internet")
+cursor.execute("use Internet")
+cursor.execute("""create table if not exists Domain (
+    id int auto_increment primary key, 
+    domain varchar(128) not null, 
+    ip varchar(128) not null)""")
+
+for i in result:
+    cursor.execute("insert into Domain (domain, ip) VALUES (%s, %s)", (i["name"], i["ip"]))
+    
+connection.commit()
+connection.close()
+    
+
+            
---
+++ setting.py
@@ -0,0 +1,5 @@
+host = "localhost"
+user = "root"
+password = "0305"
+port = "3306"
+db_name = "library"
